46edf821a3e7041c972afd1825b76980dc3ccb0b
Isac Souza Filho <souzafilhoisac@gmail.com>
2024-04-14 10:05:42 -0300

    zod type provider and using cors

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,24 @@ import fastify from "fastify";
 
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
-import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+//dessa maneira qualquer url pode acessar essa api 
+//em ambiente de produção pode não ser a melhor forma de configurar o cors
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(fastifySwagger, {
   swagger: {
@@ -40,7 +48,8 @@ app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
 
+app.setErrorHandler(errorHandler)
 
-app.listen({port: 3000}).then(() => {
+app.listen({port: 3000, host: '0.0.0.0'}).then(() => {
   console.log("HTTP server running")
 })
